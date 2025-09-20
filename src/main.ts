b9aeb064d0ed7081c5734b37965962c52d93a7d0
Jinwoo Han <qew8502@gmail.com>
2025-09-21 01:16:14 +0900

    fix: cors default

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
     origin:
       process.env.NODE_ENV === 'production'
         ? ['https://codecat-otto.shop', 'https://www.codecat-otto.shop']
-        : [process.env.FRONTEND_URL || 'http://localhost:3000'],
+        : [process.env.FRONTEND_URL || 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
